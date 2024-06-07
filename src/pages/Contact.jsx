1cf4574480f88c06a60d2340eed3fede05abe0a8
Fasika Abera <fasikabera1@gmail.com>
2024-06-07 17:21:32 +0300

    email part is under progress

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Secheader from "./common/Secondaryheader"
 import CallOutlinedIcon from '@material-ui/icons/CallOutlined';
 import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+	const [message, setMessage] = useState('');
+
+	const handleSubmit = (e)=> {
+		e.preventDefault();
+
+		const serviceId='service_5gh9wwf';
+		const templateId='template_klzrmwn';
+		const publicKey='user_GchVwmvQGhyA3aTqusDk5';
+
+		const templateParams = {
+			from_name:name,
+			from_email:email,
+			to_phone:phone,
+			to_name:'Web Wizard',
+			message:message,
+		};
+
+		emailjs.send(serviceId, templateId, templateParams, publicKey)
+		.then ((response)=>{
+			console.log('Email sent successfully!', response);
+				setName('');
+				setEmail('');
+				setPhone('');
+				setMessage('');
+			
+		}). catch((error)=>{
+			console.error('Error sending email:', error);
+		});
+	}
 	return (
 		<>
 
@@ -16,22 +49,22 @@ const Contact = () => {
 			<section>
 			<div className="container row py-5 m-auto">
 				<div className="col-md-6 d-flex align-items-center justify-content-center order-1">
-					<form className="col-lg-8 col-12 col-md-10 text-center">
+					<form onSubmit={handleSubmit} className="col-lg-8 col-12 col-md-10 text-center emailForm">
 						<p className="display-6 text-center mb-2">Reach us</p>
 						<p>Our team is happy to answer your questions.</p>
 						<div className="mb-3">
-						  <input type="text" className="form-control" id="name" placeholder="Your fullname" required/>
+						  <input value={name} onChange={(e)=>setName(e.target.value)} type="text" className="form-control" id="name" placeholder="Your fullname" required/>
 						</div>
 						<div className="mb-3">
-						  <input type="email" className="form-control" id="email" placeholder="E-mail address" required/>
+						  <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" className="form-control" id="email" placeholder="E-mail address" required/>
 						</div>
 						<div className="mb-3">
-						  <input type="tel" className="form-control" id="number" placeholder="Contact number" required/>
+						  <input value={phone} onChange={(e)=>setPhone(e.target.value)} type="tel" className="form-control" id="number" placeholder="Contact number" required/>
 						</div>
 						<div className="mb-3">
-						  <textarea className="form-control form_textarea" id="message" placeholder="Your message" rows="3"></textarea>
+						  <textarea value={message} onChange={(e)=>setMessage(e.target.value)} className="form-control form_textarea" id="message" placeholder="Your message" rows="3"></textarea>
 						</div>
-						<button className="btn btn-lg btn_custom btn_shadow col-12">Send</button>
+						<button type="submit" className="btn btn-lg btn_custom btn_shadow col-12">Send</button>
 					</form>
 				</div>
 
