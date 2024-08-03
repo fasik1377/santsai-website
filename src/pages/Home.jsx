915a92bfd9593936e07e3a2b1dc7297c0bd3ab76
Fasika Abera <fasikabera1@gmail.com>
2024-08-03 18:55:12 +0300

    push website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ import cplus from "../images/tech/Cplus.png"
 import javascript from "../images/tech/javascript.png"
 import tailwind from "../images/tech/tailwind.png"
 import odoo from "../images/tech/odoo.png"
-import product1 from "../images/process.png"
+import product1 from "../images/software_process.jpg"
 import BusinessIcon from '@material-ui/icons/Smartphone';
 import WebIcon from '@material-ui/icons/DesktopWindows';
 import ECommerce from '@material-ui/icons/ShoppingBasket';
@@ -30,53 +30,13 @@ import UI from '@material-ui/icons/GraphicEq';
 import BannerAnim, { Element } from 'rc-banner-anim';
 import TweenOne from 'rc-tween-one';
 const BgElement = Element.BgElement;
-
+// responsiveness is done
 const Home = () => {
 
 	return (
 		<>
-			<section className="sec_1">
-				<div className="sec_bg_home">
-					<div className="container banner_h">
-						<div class="eye"></div>
-						<div class="circle-1"></div>
-						<div class="circle-2"></div>
-						<div class="circle-3"></div>
-						<div class="circle-4"></div>
-						<div class="circle-5"></div>
-						<div class="circle-6"></div>
-						<div class="circle-7"></div>
-						<div class="circle-8"></div>
-						<div class="circle-9"></div>
-						<div class="circle-10"></div>
-						<div class="circle-11"></div>
-						<div class="circle-12"></div>
-						<div class="circle-13"></div>
-						<div class="circle-14"></div>
-
-						<div class="glitch"></div>
-
-						<div class="fragment-1"></div>
-						<div class="fragment-2"></div>
-						<div class="fragment-3"></div>
-
-
-						<svg width="190" height="190" viewBox="0 0 190 190" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<defs>
-								<clipPath id='bagel1'>
-									<path fill-rule="evenodd" clip-rule="evenodd" d="M95 190C147.467 190 190 147.467 190 95C190 42.533 147.467 0 95 0C42.533 0 0 42.533 0 95C0 147.467 42.533 190 95 190ZM95 120C108.807 120 120 108.807 120 95C120 81.1929 108.807 70 95 70C81.1929 70 70 81.1929 70 95C70 108.807 81.1929 120 95 120Z" />
-								</clipPath>
-								<clipPath id='bagel2'>
-									<path fill-rule="evenodd" clip-rule="evenodd" d="M71 142C110.212 142 142 110.212 142 71C142 31.7878 110.212 0 71 0C31.7878 0 0 31.7878 0 71C0 110.212 31.7878 142 71 142ZM71 139C108.555 139 139 108.555 139 71C139 33.4446 108.555 3 71 3C33.4446 3 3 33.4446 3 71C3 108.555 33.4446 139 71 139Z" />
-								</clipPath>
-								<clipPath id='bagel3'>
-									<path fill-rule="evenodd" clip-rule="evenodd" d="M60 120C93.1372 120 120 93.1372 120 60C120 26.8628 93.1372 0 60 0C26.8628 0 0 26.8628 0 60C0 93.1372 26.8628 120 60 120ZM60 115C90.3757 115 115 90.3757 115 60C115 29.6243 90.3757 5 60 5C29.6243 5 5 29.6243 5 60C5 90.3757 29.6243 115 60 115Z" />
-								</clipPath>
-								<clipPath id='bagel4'>
-									<path fill-rule="evenodd" clip-rule="evenodd" d="M38 76C58.9868 76 76 58.9868 76 38C76 17.0132 58.9868 0 38 0C17.0132 0 0 17.0132 0 38C0 58.9868 17.0132 76 38 76ZM38 72C56.7777 72 72 56.7776 72 38C72 19.2224 56.7777 4 38 4C19.2223 4 4 19.2224 4 38C4 56.7776 19.2223 72 38 72Z" />
-								</clipPath>
-							</defs>
-						</svg>
+			<section className="sec">
+				<div className="sec_bg">
 						<BannerAnim prefixCls="banner-user" autoPlay>
 							<Element
 								prefixCls="banner-user-elem"
@@ -89,13 +49,13 @@ const Home = () => {
 										background: '#364D79',
 									}}
 								/>
-								<TweenOne className="banner-user-title tween_width" animation={{ y: 30, opacity: 0, type: 'from' }}>
-								<b>Develop Mobile Apps for Profitability, Productivity, and Other Purposes</b>	
+								<TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+								<b>Develop Mobile Apps for Profitability, </b> <br /> <b> Productivity, and Other Purposes</b>	
 								</TweenOne>
-								<TweenOne className="banner-user-text tween_width"
+								<TweenOne className="banner-user-text"
 									animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
 								>
-									<p className="tween_padding">We provide end-to-end services for iOS and Android mobile applications.
+									<p className="banner_sub_title">We provide end-to-end services for iOS and Android mobile applications.
 										<br />
 										We can assist you in developing software and apps for mobile devices.</p>
 								</TweenOne>
@@ -113,14 +73,14 @@ const Home = () => {
 										}}
 									/>
 								</div>
-								<TweenOne className="banner-user-title tween_width" animation={{ y: 30, opacity: 0, type: 'from' }}>
-								<b>Developing Pixel-Perfect Websites
+								<TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+								<b>Developing Pixel-Perfect Websites </b> <br /> <b>
 								With a Splash of Originality</b>	
 								</TweenOne>
-								<TweenOne className="banner-user-text tween_width tween_padding"
+								<TweenOne className="banner-user-text"
 									animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
 								>
-									<p className="tween_paddings">
+									<p className="banner_sub_title">
 										We design stunningly distinctive websites
 										<br />
 										that captivate visitors.</p>
@@ -140,13 +100,13 @@ const Home = () => {
 										}}
 									/>
 								</div>
-								<TweenOne className="banner-user-title tween_width" animation={{ y: 30, opacity: 0, type: 'from' }}>
+								<TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
 								<b>Encourage a &nbsp;Genuine Bond.</b>	
 								</TweenOne>
-								<TweenOne className="banner-user-text tween_width"
+								<TweenOne className="banner-user-text"
 									animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
 								>
-									<p className="tween_padding">
+									<p className="banner_sub_title">
 										Custom-made, expert,
 										<br />  reasonably priced, and efficient web solutions. <br /><br />
 										Given our exceptional professionalism, we are able to provide our esteemed clients in other countries with excellent
@@ -167,18 +127,17 @@ const Home = () => {
 										}}
 									/>
 								</div>
-								<TweenOne className="banner-user-title tween_width" animation={{ y: 30, opacity: 0, type: 'from' }}>
-								<b>Incredibly Captivating Digital Marketing At a Reasonable Price Point.</b>	
+								<TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+								<b>Incredibly Captivating Digital Marketing </b> <br /> <b> At a Reasonable Price Point.</b>	
 								</TweenOne>
-								<TweenOne className="banner-user-text tween_width"
+								<TweenOne className="banner-user-text"
 									animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
 								>
-								<p className="tween_padding">We create incredible digital marketing strategies to strengthen your brand.</p>	
+								<p className="banner_sub_title">We create incredible digital marketing strategies to strengthen your brand.</p>	
 									<br />
 								</TweenOne>
 							</Element>
 						</BannerAnim>
-					</div>
 				</div>
 			</section>
 
@@ -228,7 +187,7 @@ const Home = () => {
 			</section>
 			<section>
 				<div className="col-12 col-md-9 col-lg-6 container d-flex align-items-center">
-					<table className="tween_width">
+					<table className="">
 						<tr>
 							<p className="display-6 mb-1 fw-bold our">Technology at a <span className="span_portfolio">Glance</span> </p>
 						</tr>
@@ -298,10 +257,10 @@ const Home = () => {
 						<p className="display-6 mb-1"><b>Our <span className="main_color">Process</span></b></p>
 						<p className="text-muted">We take great pride in providing our clients with ideal solutions tailored to their specific issues. To earn our clients' loyalty as business partners, we provide them with personal service.</p>
 					</div>
-					<div className="row gy-3 d-flex align-items-center">
-						<div className="col-md-3 text-center">
+					<div className="row gy-12 d-flex align-items-center">
+						<div className="col-md-12 col-lg-12 text-center">
 							<div className="snip1534">
-								<img className="img-fluid" alt="product1" src={product1} />
+								<img className="img-responsive img-fluid" alt="product1" src={product1} />
 							</div>
 						</div>
 					</div>
