ecb57c9117fbb52844a51c33a25ba5fa7366f456
Fasika Abera <fasikabera1@gmail.com>
2024-09-25 12:00:26 +0300

    Footer update and push John change

--- src/pages/common/Footer.jsx
+++ src/pages/common/Footer.jsx
@@ -7,8 +7,10 @@ const Footer = () => {
 	return (
 		<>
 			<footer>
+				{/* Show me local is added */}
 				<div className="showmelocal">
-				<a href="https://www.showmelocal.com/profile.aspx?bid=37667698" target="_blank"><img src="https://www.showmelocal.com/showmelocal-member-37667698" alt="Santsai Corporation Pvt. Ltd..,Software Company,Jamshedpur,Jharkhand" /></a></div>
+				<a href="https://www.showmelocal.com/profile.aspx?bid=37667698" target="_blank"><img src="https://www.showmelocal.com/showmelocal-member-37667698" alt="Santsai Corporation Pvt. Ltd..,Software Company,Jamshedpur,Jharkhand" /></a>
+				</div>
 				<div className="container d-lg-flex align-items-center justify-content-center py-3 medium text-center ms-auto">
 					<div className="text-white medium we_appreciate" style={{ marginTop: '-120px' }}>We appreciate you choosing to contact us.</div>
 				</div>
